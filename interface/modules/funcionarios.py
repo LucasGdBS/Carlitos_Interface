83c5eb3720a8f91e2dabfb526f80f8a221bec9ef
Carlos3du <cepc@cesar.school>
2024-05-15 23:40:07 -0300

    feat: delete a functionary and fix some interface issues

--- interface/modules/funcionarios.py
+++ interface/modules/funcionarios.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-
+import time
 
 
 def fetch_funcionario():
@@ -27,6 +27,17 @@ def fetch_funcionario_by_nome(nome):
         elif response.status_code == 404:
             return []
 
+    except Exception:
+        return []
+
+def fetch_funcionario_by_cpf(cpf):
+    url = f"http://localhost:8080/funcionarios/buscar-por-cpf?cpf={cpf}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return []
     except Exception:
         return []
 
@@ -63,6 +74,16 @@ def edit_funcionario_by_cpf(cpf, nome, salario):
     except Exception:
         return False
 
+def delete_funcionario(cpf):
+    url = f"http://localhost:8080/funcionarios/{cpf}"
+    try:
+        response = requests.delete(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
 
 
 
@@ -73,18 +94,7 @@ def page_funcionario():
 
     if "options" not in st.session_state:
         st.session_state.options = "cadastrar"
-
-
-    cols = st.columns(3)
-    with cols[0]:
-        if st.button("Cadastrar",use_container_width=True):
-            st.session_state.options = "cadastrar"
-    with cols[1]:
-        if st.button("Editar", use_container_width=True):
-            st.session_state.options = "editar"
-    with cols[2]:
-        if st.button("Deletar", use_container_width=True):
-            st.session_state.options = "deletar"
+    st.radio("Operações:", ["cadastrar", "editar", "deletar"], key="options", horizontal=True)
 
 
 
@@ -115,31 +125,51 @@ def page_funcionario():
                         st.toast("Erro ao cadastrar funcionário", icon="⚠️")
 
 
+
     # * Editar Funcionario
     if st.session_state.options == "editar":
-        with st.form("editar_funcionario", clear_on_submit=True):
-            st.subheader("Editar Funcionário")
 
-            cols = st.columns(3)
-            with cols[0]:
-                cpf = st.text_input("CPF")
-            with cols[1]:
-                nome = st.text_input("Nome")
-            with cols[2]:
-                salario = st.text_input("Salario")
-            
-            col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
+        cpf_func = st.text_input("CPF do funcionário")
+        selected_func = fetch_funcionario_by_cpf(cpf_func)
+        
 
-            with col1:
-                if st.form_submit_button("Cancelar", type="primary", use_container_width=True):
-                    pass
-            with col2:
-                if st.form_submit_button("Editar", use_container_width=True):
-                    edit_func = edit_funcionario_by_cpf(cpf, nome, salario)
-                    if edit_func:
-                        st.toast("Funcionário editado com sucesso", icon="🎉")
-                    else:
-                        st.toast("Erro ao editar funcionário", icon="⚠️")
+        if "changed" not in st.session_state:
+            st.session_state.changed = False
+        if st.session_state.changed:
+            st.session_state.changed = False
+            st.toast("Funcionário editado com sucesso", icon="🎉")
+
+
+        if selected_func:
+
+            with st.form("editar_funcionario", clear_on_submit=True):
+                st.subheader("Editar Funcionário")
+    
+                cols = st.columns(3)
+                with cols[0]:
+                    cpf = st.text_input("CPF",value=cpf_func, disabled=True)
+                with cols[1]:
+                    nome = st.text_input("Nome", value=selected_func["nome"])
+                with cols[2]:
+                    salario = st.text_input("Salario", value=selected_func["salario"])
+                
+
+                col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
+                with col1:
+                    if st.form_submit_button("Cancelar", type="primary", use_container_width=True):
+                        pass
+                with col2:
+                    if st.form_submit_button("Editar", use_container_width=True):
+                        edit_func = edit_funcionario_by_cpf(cpf, nome, salario)
+                        if edit_func:
+                            st.session_state.changed = True
+                            st.rerun()
+                        else:
+                            st.toast("Erro ao editar funcionário", icon="⚠️")
+
+        elif selected_func == [] and cpf_func != "":
+            st.toast("Funcionário não encontrado", icon="⚠️")
+
 
 
     # * Deletar Funcionario
@@ -149,14 +179,19 @@ def page_funcionario():
 
             cpf = st.text_input("CPF")
 
-            col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
 
+            col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
             with col1:
                 if st.form_submit_button("Cancelar", type="primary", use_container_width=True):
                     pass
             with col2:
                 if st.form_submit_button("Deletar", use_container_width=True):
-                    st.toast("Funcionário deletado com sucesso", icon="🎉")
+                    delete_func = delete_funcionario(cpf)
+                    if delete_func:
+                        st.toast("Funcionário deletado com sucesso", icon="🎉")
+                    else:
+                        st.toast("Erro ao deletar funcionário", icon="⚠️")
+                    
 
 
 
@@ -164,15 +199,23 @@ def page_funcionario():
     st.subheader("Lista de Funcionários")
 
 
+    if "search" not in st.session_state:
+        st.session_state.search = "nome"
+    st.radio("Buscar por:", ["nome", "cpf"], key="search", horizontal=True)
+
+
+    id_func = st.text_input(" ", label_visibility="collapsed", placeholder=f"{st.session_state.search} do funcionário")
+
 
     # * Exibir por nome ou exibir todos
-    name_func = st.text_input("Buscar por nome")
-    if name_func:
-        data_func = fetch_funcionario_by_nome(name_func)
+    if id_func:
+        if st.session_state.search == "nome":
+            data_func = fetch_funcionario_by_nome(id_func)
+        elif st.session_state.search == "cpf":
+            data_func = fetch_funcionario_by_cpf(id_func)
     else:
         data_func = fetch_funcionario()
-
-
+    
 
     if data_func == []:
         st.toast("Nenhum funcionário encontrado", icon="⚠️")
@@ -180,4 +223,4 @@ def page_funcionario():
         df_func = pd.DataFrame(data_func)
         df_func.columns = ["CPF", "Nome", "Salário"]
         st.dataframe(df_func, hide_index=True, use_container_width=True)   
-    
+
