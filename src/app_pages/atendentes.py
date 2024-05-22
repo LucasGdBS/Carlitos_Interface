0e08abb40f87b0f0e0f835c6a35a1b5887754390
Carlos3du <cepc@cesar.school>
2024-05-22 18:50:30 -0300

    feat: page atendente (have to test)

--- src/app_pages/atendentes.py
+++ src/app_pages/atendentes.py
@@ -1,45 +1,203 @@
 import streamlit as st
 import pandas as pd
+from utils.page_modules import clear_input
+from utils.aten_modules import fetch_atendente, fetch_atendente_by_cpf, fetch_atendente_by_nome, fetch_atendente_by_geren_cpf, create_atendente, edit_atendente_by_cpf, delete_atendente
+from utils.func_modules import fetch_funcionario
 
 
+
+# * Página dos atendentes
 def page_atendente():
     st.title("☎️ Atendentes")
     st.markdown("<br>", unsafe_allow_html=True)
+
+
+    if "options" not in st.session_state:
+        st.session_state.options = "atribuir cargo"
+    st.radio("Operações:", ["atribuir cargo", "editar", "deletar"], key="options", horizontal=True)
+
+
+    values = fetch_funcionario()
+    values = [i["cpf"] for i in values]
+
+
+
+    cpf_aten = st.selectbox("CPF do atendente", values, placeholder="cpf do atendente", label_visibility="collapsed", index=0)
+    selected_aten = fetch_atendente_by_cpf(cpf_aten)
     
+        
+                
 
-    st.subheader("Lista de Atendentes")
+    # * Criar Atendente
+    if st.session_state.options == "atribuir cargo":
+
+        if "changed" not in st.session_state:
+            st.session_state.changed = False
+        if st.session_state.changed:
+            st.session_state.changed = False
+            st.toast("Cargo de atendente atribuído com sucesso", icon="🎉")
+
+        if selected_aten:
+            with st.form("atribuir_cargo", clear_on_submit= True):
+                st.subheader("Atribuir Cargo de Atendente")
+
+                cols = st.columns(3)
+                with cols[0]:
+                    cpf = st.text_input("CPF",value=cpf_aten, disabled=True)
+                with cols[1]:
+                    cpf_gerente = st.text_input("CPF do Gerente")
+                with cols[2]:
+                    turno =  st.radio("Turno:", ["MANHÃ", "TARDE", "NOITE"], horizontal=True)
+                
+                
+                col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
+
+                with col1:
+                    if st.form_submit_button("Atribuir", use_container_width=True):
+                        new_aten = create_atendente(cpf, cpf_gerente, turno.upper())	
+                        if new_aten:
+                            st.session_state.changed = True
+                            st.rerun()
+                        else:
+                            st.toast("Erro ao atribuir cargo", icon="⚠️")
+                with col2:
+                    if st.form_submit_button("Cancelar", type="primary", use_container_width=True):
+                        pass
 
 
-    sample_data = [
-        {
-            "nome": "João",
-            "cpf": "123.456.789-00",
-            "salario": "R$ 1.500,00",
-            "cargo": "Atendente",
-        },
-        {
-            "nome": "Ana",
-            "cpf": "654.321.987-00",
-            "salario": "R$ 2.000,00",
-            "cargo": "Atendente",
-        },
-        {
-            "nome": "Maria",
-            "cpf": "987.654.321-00",
-            "salario": "R$ 2.500,00",
-            "cargo": "Atendente",
-        },
-        {
-            "nome": "José",
-            "cpf": "456.789.123-00",
-            "salario": "R$ 1.800,00",
-            "cargo": "Atendente",
-        }
-    ]
+    # * Editar atendente
+    if st.session_state.options == "editar":
+
+        if "changed" not in st.session_state:
+            st.session_state.changed = False
+        if st.session_state.changed:
+            st.session_state.changed = False
+            st.toast("Atendente editado com sucesso", icon="🎉")
 
 
-    df_atend = pd.DataFrame(sample_data)
-    df_atend.columns = ["Nome", "CPF", "Salario", "Cargo"]
-
-    st.dataframe(df_atend, hide_index=True, use_container_width=True)   
+        if selected_aten:
+            with st.form("editar_atendente", clear_on_submit=True):
+                st.subheader("Editar atendente")
     
+                cols = st.columns(3)
+                with cols[0]:
+                    cpf = st.text_input("CPF",value=cpf_aten, disabled=True)
+                with cols[1]:
+                    cpf_gerente = st.text_input("CPF do Gerente", value=selected_aten["cpf_gerente"], disabled=True)
+                with cols[2]:
+                    if selected_aten["turno"] == "MANHA":
+                        ind = 0
+                    elif selected_aten["turno"] == "TARDE":
+                        ind = 1
+                    else:
+                        ind = 2
+                    turno = st.radio("Turno:", ["MANHÃ", "TARDE", "NOITE"], index=ind, horizontal=True)
+                
+
+                col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
+                with col1:
+                    if st.form_submit_button("Editar", use_container_width=True):
+                        edit_moto = edit_atendente_by_cpf(cpf, turno.upper())
+                        if edit_moto:
+                            st.session_state.changed = True
+                            st.rerun()
+                        else:
+                            st.toast("Erro ao editar atendente", icon="⚠️")
+                with col2:
+                    if st.form_submit_button("Cancelar", type="primary", use_container_width=True):
+                        pass
+
+        elif selected_aten == [] and cpf_aten != "":
+            st.toast("atendente não encontrado", icon="⚠️")
+
+
+
+    # * Deletar atendente
+    if st.session_state.options == "deletar":
+
+        if "deleted" not in st.session_state:
+            st.session_state.deleted = False
+        if st.session_state.deleted:
+            st.session_state.deleted = False
+
+            st.toast("atendente deletado com sucesso", icon="🎉")
+        
+
+        if selected_aten:
+            with st.form("deletar_atendente", clear_on_submit=True):
+                st.subheader("Deletar atendente")
+
+                cols = st.columns(3)
+                with cols[0]:
+                    cpf = st.text_input("CPF", value=cpf_aten, disabled=True)
+                with cols[1]:
+                    cpf_gerente = st.text_input("CPF do Gerente", value=selected_aten["cpf_gerente"], disabled=True)
+                with cols[2]:
+                    if selected_aten["turno"] == "MANHA":
+                        ind = 0
+                    elif selected_aten["turno"] == "TARDE":
+                        ind = 1
+                    else:
+                        ind = 2
+                    turno = st.radio("Turno:", ["Manhã", "Tarde", "Noite"], index=ind, horizontal=True, disabled=True)
+
+
+                col_space, col1, col2 = st.columns([7, 1, 1], gap="small")
+
+                with col1:
+                    if st.form_submit_button("Deletar", use_container_width=True):
+                        delete_func = delete_atendente(cpf)
+                        if delete_func:
+                            st.session_state.deleted = True
+                            st.rerun()
+                        else:
+                            st.toast("Erro ao deletar atendente", icon="⚠️")
+                with col2:
+                    if st.form_submit_button("Cancelar",type='primary', use_container_width=True):
+                        pass
+        elif selected_aten == [] and cpf_aten != "" and st.session_state.deleted == True:
+            st.toast("atendente não encontrado", icon="⚠️")                
+
+
+
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.subheader("Lista dos Atendentes")
+
+
+    if "search" not in st.session_state:
+        st.session_state.search = "nome"
+    st.radio("Buscar por:", ["nome", "cpf", "cpf do gerente"], key="search", horizontal=True)
+
+
+    col1, col2, col3 = st.columns([8, 1, 1])
+    with col1:
+        id_aten = st.text_input("ID do atendente", label_visibility="collapsed", placeholder=f"{st.session_state.search} do atendente", key="search_input")
+    
+    with col2:
+        if st.button("Buscar", use_container_width=True, key="bottom_search_button"):
+            pass
+    with col3:
+        st.button("Limpar", on_click=clear_input, use_container_width=True)
+        
+
+    # * Buscar por nome, por cpf ou exibir todos
+    if id_aten:
+        if st.session_state.search == "nome":
+            data_aten = fetch_atendente_by_nome(id_aten)
+            df_aten = pd.DataFrame(data_aten)
+        if st.session_state.search == "cpf":
+            data_aten = fetch_atendente_by_cpf(id_aten)
+            df_aten = pd.DataFrame([data_aten]) 
+        if st.session_state.search == "cpf do gerente":
+            data_aten = fetch_atendente_by_geren_cpf(id_aten)
+            df_aten = pd.DataFrame(data_aten)
+    else:
+        data_aten = fetch_atendente()
+        df_aten = pd.DataFrame(data_aten)
+
+    if data_aten == []:
+        st.error("Nenhum atendente encontrado")
+    else:
+        #df_aten.columns = ["CPF", "Nome", "Salário", "Turno", "CPF Gerente", "Nome Gerente"]
+        st.dataframe(df_aten, hide_index=True, use_container_width=True)   
+        
---
+++ src/utils/aten_modules.py
@@ -0,0 +1,96 @@
+import requests
+
+def fetch_atendente():
+    url = "http://localhost:8080/atendentes/"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return []
+        
+    except Exception:
+        return []
+
+
+def fetch_atendente_by_nome(nome):
+    url = f"http://localhost:8080/atendentes/buscar-por-nome?nome={nome}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return []
+        
+    except Exception:
+        return []
+
+
+def fetch_atendente_by_cpf(cpf):
+    url = f"http://localhost:8080/atendentes/buscar-por-cpf?cpf={cpf}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return []
+        
+    except Exception:
+        return []
+
+
+def fetch_atendente_by_geren_cpf(cpf):
+    url = f"http://localhost:8080/atendentes/buscar-por-gerente?cpf={cpf}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            return []
+        
+    except Exception:
+        return []
+
+
+def create_atendente(cpf, cpf_gerente, turno):
+    url = "http://localhost:8080/atendentes/"
+    data = {
+        "cpf": cpf,
+        "gpf_gerente": cpf_gerente,
+        "turno": turno
+    }
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 201:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+def edit_atendente_by_cpf(cpf, turno):
+    url = f"http://localhost:8080/atendentes/{cpf}"
+    data = {
+        "turno": turno
+    }
+    try:
+        response = requests.put(url, json=data)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+def delete_atendente(cpf):
+    url = f"http://localhost:8080/atendentes/{cpf}"
+    try:
+        response = requests.delete(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
